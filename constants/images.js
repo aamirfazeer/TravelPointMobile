54f1b8b6e1550b0fe8c019183d4567d2c0e0244d
ABC Silva <afeefcareem6@gmail.com>
2024-07-17 00:03:12 +0530

    profile image updated

--- constants/images.js
+++ constants/images.js
@@ -10,16 +10,12 @@ import travelpoint_logo from "../assets/images/travelpoint_name.png";
 import hipster from "../assets/images/hipster.png";
 import rental from "../assets/images/rental.png";
 import equipment from "../assets/images/equipment.png";
-<<<<<<< HEAD
 import travel1 from "../assets/images/travel1.png";
 import travel2 from "../assets/images/travel2.png";
 import travel3 from "../assets/images/travel3.png";
 import person1 from "../assets/images/person1.png";
-
-export default { profile, thumbnail, empty, travelpoint_image, travelpoint_logo, hipster, rental, equipment, travel1, travel2, travel3, person1 };
-=======
 import profile1 from "../assets/images/profile1.png";
 
+export default { profile, thumbnail, empty, travelpoint_image, travelpoint_logo, hipster, rental, equipment, travel1, travel2, travel3, person1, profile1 };
 
-export default { profile, thumbnail, cards, path, logo, logoSmall, empty, travelpoint_image, travelpoint_logo, hipster, rental, equipment, profile1};
->>>>>>> 2745255c01ebbacab1e63998b2fd853bcb3e71e7
+
