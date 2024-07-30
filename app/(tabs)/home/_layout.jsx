3f8b19cacb031ec797762881f67b40b242ae3961
Mushahid-Muzammir <mushahidmuzammir@gmail.com>
2024-07-30 21:33:49 +0530

    Added Equipment Provider Pages

--- app/(tabs)/home/_layout.jsx
+++ app/(tabs)/home/_layout.jsx
@@ -4,6 +4,8 @@ const Stacklayout = () => {
   return (
     <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="messages" options={{ headerShown: false }} />
+
     </Stack>
   )
 }
